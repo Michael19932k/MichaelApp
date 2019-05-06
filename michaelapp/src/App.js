490c415fe9268b57ca6d8f951b7a911128f4823d
tal yaron <tal.yaron@gmail.com>
2019-05-06 18:38:43 +0300

    fix

--- michaelapp/src/App.js
+++ michaelapp/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+
 class App extends React.Component {
   render() {
     return (
--- michaelapp/src/Header.js
+++ michaelapp/src/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './header.css';
+import './Header.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Header extends React.Component {
+    render() {
+      return (
+        <div class='header'>Header</div>
+      );
+    }
+  }
+  
+  export default Header;
 
-
-<div class='header'></div>
