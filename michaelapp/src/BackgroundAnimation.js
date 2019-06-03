a70dcafc1eecb4471cbd49da3fdd534c53f7801c
Michael <pucika2k@gmail.com>
2019-06-03 20:52:51 +0300

    change

--- michaelapp/src/BackgroundAnimation.js
+++ michaelapp/src/BackgroundAnimation.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './BackgroundAnimation.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { defaultCipherList } from 'constants';
 
 
 function BackgroundAnimation(){
---
+++ michaelapp/src/linkWindow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './linkWindow.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { defaultCipherList } from 'constants';
+
+
+function linkWindow(){
+    return(
+<div></div>    
+
+
+
+
+    )
+    }
+    
+
+
+export default linkWindow;
